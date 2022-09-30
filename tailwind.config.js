522ea49c861ec018723adf4b8e46587f012f6b93
Truth-hash <86872654+Truth-hash@users.noreply.github.com>
2022-09-30 05:11:16 +0000

    update readme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: ["./index.html", "./assets/js/**/*.js"],
+  content: ["./index.html"],
   theme: {
     extend: {
       fontSize: {
